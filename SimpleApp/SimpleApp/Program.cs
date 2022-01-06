20045a6bb972f2f4ec12c9fce08de16b082c30eb
Matthew Suddaby <msuddaby2@nait.ca>
2022-01-06 16:19:34 -0700

    added new write methods

--- SimpleApp/SimpleApp/Program.cs
+++ SimpleApp/SimpleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("my name jeff");
+Console.WriteLine("is it really?");
+Console.Write("No.");
+Console.Write(" It is not.");
